8d1f875c044dd53c772d6408f1b0a9dec5adead3
Preeti-agan <vijayagan92@gmail.com>
2025-01-07 16:37:27 +0530

    First upload

---
+++ src/About/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './About.css'
+const About = () => {
+  return (
+    <div className='about'>
+      <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/portal/m/seo/App_download_banner.png" width="1263.33" height="247.1"></img>
+    </div>
+  )
+}
+
+export default About
