56cfa4d28707bd35248bb97872f85ac11b5da70e
Manpreet Singh <manpreet.singh@bjshomedelivery.com>
2025-01-15 16:15:57 +0530

    header fixes

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from "react";
 import "./Home.css";
 const Home = () => {
@@ -10,14 +11,13 @@ const Home = () => {
               className="swiggy"
               src="https://res.cloudinary.com/dutdah0l9/image/upload/v1720058694/Swiggy_logo_bml6he.png"
               width="60px"
-              
             ></img>
           </div>
           <div className="two">
             <a href="#" className="corporate">
               Swiggy Corporate
             </a>
-            
+
             <a href="#" className="partner">
               Partner with us
             </a>
@@ -26,7 +26,6 @@ const Home = () => {
               <img
                 src="https://cdn-icons-png.flaticon.com/128/15485/15485092.png"
                 height="20px"
-                
               ></img>
             </button>
             <button className="sign">Sign In</button>
@@ -39,50 +38,46 @@ const Home = () => {
           <img
             src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/portal/testing/seo-home/Veggies_new.png"
             height="550px"
-           
           ></img>
         </div>
         <div className="heading">
           <h1 className="order">
             Order food & groceries. Discover best restaurants. Swiggy it!
           </h1>
+          <div className="section3">
+            <div className="btn1">
+              <img
+                className="location"
+                src="https://cdn-icons-png.flaticon.com/128/929/929426.png"
+                height="17px"
+              ></img>{" "}
+              Enter your delivery location
+              <img
+                className="arrow"
+                src="https://cdn-icons-png.flaticon.com/128/2985/2985150.png"
+                height="12px"
+                width="18px"
+              ></img>
+            </div>
+
+            <div className="btn2">
+              {" "}
+              Search for restaurant, item or more
+              <img
+                className="search"
+                src="https://cdn-icons-png.flaticon.com/128/11741/11741045.png"
+                height="19px"
+                width="19px"
+              ></img>{" "}
+            </div>
+          </div>
         </div>
 
         <div className="imgright">
           <img
             src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/portal/testing/seo-home/Sushi_replace.png"
-           height="550px"
-            
+            height="550px"
           ></img>
-        </div>
-      </div>
-
-      <div className="section3">
-        <div className="btn1">
-          <img
-            className="location"
-            src="https://cdn-icons-png.flaticon.com/128/929/929426.png"
-            height="17px"
-            
-          ></img>{" "}
-          Enter your delivery location
-          <img
-            className="arrow"
-            src="https://cdn-icons-png.flaticon.com/128/2985/2985150.png"
-            height="12px"
-            width="18px"
-          ></img>
-        </div>
-
-        <div className="btn2">
-          {" "}
-          Search for restaurant, item or more
-          <img
-            className="search"
-            src="https://cdn-icons-png.flaticon.com/128/11741/11741045.png"
-            height="19px"
-            width="19px"
-          ></img>{" "}
         </div>
       </div>
 
@@ -90,21 +85,19 @@ const Home = () => {
         <div className="temp1">
           <img
             src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/8/1/fa01e85b-3057-482d-9523-5289722b1df2_Food4BU.png"
-           height="340.78px"
-            
+            height="340.78px"
           ></img>
         </div>
         <div className="temp2">
           <img
             src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/8/16/ca34e375-f1bd-4a2e-a3e7-0a20833be83b_IM4BU1.png"
-           
-           height="340.78px"
+            height="340.78px"
           ></img>
         </div>
         <div className="temp3">
           <img
             src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/8/1/76c30e5a-8adb-4795-bf5b-fa64e9e9e1d3_DO4BU.png"
-           height="340.78px"
+            height="340.78px"
           ></img>
         </div>
         <div className="temp4">
